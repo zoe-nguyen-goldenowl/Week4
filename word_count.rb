e86f23c89045e2fc071875189273fe2a8b36d511
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-01-04 16:28:00 +0700

    tue

--- word_count.rb
+++ word_count.rb
@@ -13,3 +13,22 @@ class Phrase
 end
 ph = Phrase.new("one fish two fish red fish blue fish")
 p ph.count
+=begin
+Given a phrase, count the occurrences of each word in that phrase.
+
+For the purposes of this exercise you can expect that a word will always be one of:
+
+    A number composed of one or more ASCII digits (ie "0" or "1234") OR
+    A simple word composed of one or more ASCII letters (ie "a" or "they") OR
+    A contraction of two simple words joined by a single apostrophe (ie "it's" or "they're")
+
+When counting words you can assume the following rules:
+
+    The count is case insensitive (ie "You", "you", and "YOU" are 3 uses of the same word)
+    The count is unordered; the tests will ignore how words and counts are ordered
+    Other than the apostrophe in a contraction all forms of punctuation are ignored
+    The words can be separated by any form of whitespace (ie "\t", "\n", " ")
+
+For example, for the phrase "That's the password: 'PASSWORD 123'!", cried the Special
+ Agent.\nSo I fled.
+=end
