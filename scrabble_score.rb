c7cb042ae451c41caed30863e0ff0a1a8d12cdf7
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-01-05 09:10:54 +0700

    tue

--- scrabble_score.rb
+++ scrabble_score.rb
@@ -1,34 +1,39 @@
-giclass Scrabble
+class Scrabble
     attr_reader :str
     def initialize(str)
         @str = str
         @s = {
             ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'] => 1,
             ['D', 'G'] => 2,
+            ['k'] => 5,
             ['B', 'C', 'M', 'P'] => 3,
             ['F', 'H', 'V', 'W', 'Y'] => 4,
-            ['k'] => 5,
             ['J', 'X'] => 8,
             ['Q', 'Z'] => 10
         }
     end
-
-    def self.score
-        p str = str.slip('')
+    
+    def score
+        str = @str.split('')
         ans = 0
-       str.each_key do |i|
+       str.each do |i|
             @s.each do |k,v|
                 if k.include?(i.upcase)
+                    p v
                     ans += v
                 end
             end
         end
         p ans
     end
-
 end
-sc = Scrabble.new('OXYAC')
+sc = Scrabble.new('alacrity')
 sc.score
+
+
+
+
+
 
 =begin
 Letter                           Value
